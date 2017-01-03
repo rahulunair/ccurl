5c59799ebb39ec40179404bb920bf6451d1c56ee
Rahul Nair <rahulunair@gmail.com>
2017-01-03 21:53:53 +0000

    updating update_token script

--- update_token.sh
+++ update_token.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+echo "Refreshes 'auth_token' in ccurl.conf by requesting a new token from openstack"
 sed -i -E "s/^auth_token.*$/$(openstack token issue  | awk 'NR==5 {print "auth_token="$4}')/g" ccurl.conf
